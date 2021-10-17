3a8dc50fd1308cee6938151543521e05bd4c42d9
Guilherme Luiz Lange <gui.luizlange@gmail.com>
2021-10-16 21:40:18 -0300

    Initial Commit - Passes on test

---
+++ src/services/DeleteTransactionService.ts
@@ -0,0 +1,24 @@
+// import AppError from '../errors/AppError';
+
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  id: string;
+}
+
+class DeleteTransactionService {
+  public async execute({ id }: Request): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const transaction = await transactionRepository.findOne({ where: { id } });
+
+    if (!transaction) {
+      throw new AppError('Transaction not found.');
+    }
+
+    await transactionRepository.remove(transaction);
+  }
+}
+
+export default DeleteTransactionService;
